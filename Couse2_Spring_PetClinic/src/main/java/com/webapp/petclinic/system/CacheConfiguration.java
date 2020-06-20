bc1becc16c1d7a8d0bbe040215642ac34c6a8254
Nay Chi <Nay Chi@isgm112>
2020-06-20 14:47:15 +0630

    commit cache test

--- Couse2_Spring_PetClinic/src/main/java/com/webapp/petclinic/system/CacheConfiguration.java
+++ Couse2_Spring_PetClinic/src/main/java/com/webapp/petclinic/system/CacheConfiguration.java
@@ -31,7 +31,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration(proxyBeanMethods = false)
 @EnableCaching
 class CacheConfiguration {
-
+//test cache
 	@Bean
 	public JCacheManagerCustomizer petclinicCacheConfigurationCustomizer() {
 		return cm -> {

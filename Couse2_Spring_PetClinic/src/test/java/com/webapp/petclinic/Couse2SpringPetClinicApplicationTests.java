f38c5100200d24203ce076e7120ac7afd3b667b4
Nay Chi <Nay Chi@isgm112>
2020-06-20 14:46:28 +0630

    First commit for Pet clinic

---
+++ Couse2_Spring_PetClinic/src/test/java/com/webapp/petclinic/Couse2SpringPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webapp.petclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Couse2SpringPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
